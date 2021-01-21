4f5fc6c5f3b8759e647dc1c1309fc3ee01e13efa
ZapAnton <wc3ft2@gmail.com>
2021-01-21 04:45:52 +0300

    01_1: Formatted source code with clang-format

--- 01_1/hello_world.cpp
+++ 01_1/hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, World!" << std:: endl;
+int main(int, char **) {
+    std::cout << "Hello, World!" << std::endl;
     const auto cout_result = std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
     return cout_result;
 }
