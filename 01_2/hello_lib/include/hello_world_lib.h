ce2771b2c08da3f63f2ebbb56c9dfb9ad6264ef1
ZapAnton <wc3ft2@gmail.com>
2021-01-22 21:53:10 +0300

    01_2: Added static and shared hello world lib implementation

---
+++ 01_2/hello_lib/include/hello_world_lib.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_WORLD_LIB_H
+#define HELLO_WORLD_LIB_H
+
+#include <string>
+
+bool hello(const std::string &user_name);
+
+#endif
---
+++ 01_2/hello_lib/src/hello_world_lib.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+
+bool hello(const std::string &user_name) {
+    const auto greet_target = user_name.empty() ? "World" : user_name;
+    std::cout << "Hello, " << greet_target << std::endl;
+    const auto cout_result = std::cout.good();
+    return cout_result;
+}
