c864625df13a9ef24a5525af2356d0f9e1e5e11b
ZapAnton <wc3ft2@gmail.com>
2021-03-02 18:33:16 +0300

    03_1: Added basic SDL window

--- 03_1/game.cpp
+++ 03_1/game.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
 
 int main() {
-    std::cout << "Game loop" << std::endl;
-    return 0;
+    const int SDL_INIT_RESULT = SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDL_INIT_RESULT != 0) {
+        const std::string error_message{SDL_GetError()};
+        std::cerr << "Failed to call SDL_Init: " << error_message << std::endl;
+        return EXIT_FAILURE;
+    }
+    SDL_Window* const window = SDL_CreateWindow(
+        "Stay Grey",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1024,
+        768,
+        SDL_WINDOW_SHOWN
+    );
+    if (window == nullptr) {
+        const std::string error_message{SDL_GetError()};
+        std::cerr << "Failed to call SDL_CreateWindow: " << error_message << std::endl;
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    bool game_is_running = true;
+    while (game_is_running) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event) != 1) {
+            continue;
+        }
+        switch (event.type) {
+            case SDL_QUIT:
+                game_is_running = false;
+                break;
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
